5513bbc898788e4293a76bcfb699c46f0fd90656
sztok7 <filip.stokovic@gmail.com>
2020-01-19 21:26:45 +0100

    Added class Path

---
+++ bioinf/src/main/java/hr/fer/bioinf/bioinf/Path.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hr.fer.bioinf.bioinf;
+
+import java.util.ArrayList;
+
+/**
+ * @author filip
+ *
+ */
+public class Path {
+	int length;
+	ArrayList<Integer> vertices = new ArrayList<Integer>(), extension_lengths = new ArrayList<Integer>();
+	
+	public Path (int length, ArrayList<Integer> vertices, ArrayList<Integer> extension_lengths) {
+		this.length = length;
+		this.vertices.addAll(vertices);
+		this.extension_lengths.addAll(extension_lengths);
+	}
+}
